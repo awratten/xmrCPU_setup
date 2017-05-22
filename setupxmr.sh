de7a9b3bf828d8cf7336f2e772a5f2f745dfe275
Anthony Wratten <awratten1@gmail.com>
2017-05-22 10:05:53 +1000

    More Concise Code for Same operation

--- setupxmr.sh
+++ setupxmr.sh
@@ -1,28 +1,6 @@
-# Update & Upgrade Apt
-sudo apt-get -y update && sudo apt-get -y upgrade
-
-# Install Required Packages via APT
-sudo apt-get -y install git wget build-essential autotools-dev libcurl3 automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev make g++
-
-# Download Latest Source of Wold9466 Miner
+cd ~
+apt-get update && apt-get install -y automake libcurl4-openssl-dev git make build-essential
 git clone https://github.com/wolf9466/cpuminer-multi
-
-# Download Latest Source of Tpruvot Miner
-# This runs a little slower than Wolf from my tests
-# git clone https://github.com/tpruvot/cpuminer-multi
-
-# Change to Miner Directory
-cd cpuminer-multi/
-
-# Remove Last Miner Compile Attempt 
-sudo make clean
-
-# Compile Miner
-./autogen.sh
-CFLAGS="-march=native" ./configure
-make
-
-# Install Miner
-sudo make install
-
-cp minerd /usr/bin/
+cd cpuminer-multi && ./autogen.sh && ./configure CFLAGS="-O3" && make
+cp ~/cpuminer-multi/minerd /usr/bin/minerd
+cd ~
